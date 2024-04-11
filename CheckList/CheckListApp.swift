3be4fa2ec704c157741bfcf1dddca6a91b9b8bdf
garyuu09 <ribereo.minami@gmail.com>
2024-04-12 00:50:03 +0900

    Initial Commit

---
+++ CheckList/CheckListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckListApp.swift
+//  CheckList
+//
+//  Created by Ryuga on 2024/04/12.
+//
+
+import SwiftUI
+
+@main
+struct CheckListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
