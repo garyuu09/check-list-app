f900bd4d7d31154291c35c4446e439b68071c4a7
garyuu09 <ribereo.minami@gmail.com>
2024-05-06 21:41:38 +0900

    feat: #30 ネットワーク接続状態を監視するクラスを作成し、ネットワーク未接続時のViewを用意した。

---
+++ CheckList/Util/NetworkMonitor.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkMonitor.swift
+//  CheckList
+//
+//  Created by Ryuga on 2024/05/06.
+//
+
+import SwiftUI
+
+struct NetworkMonitor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NetworkMonitor()
+}
